5efc51f90704c49d56c95290a067d4a5d0e0d8d4
leopbarao <leopbarao@gmail.com>
2015-10-31 19:35:54 -0200

    Teste paginacao e include mvc na camada web

--- SCT.Application.UnitTest/UnitTest1.cs
+++ SCT.Application.UnitTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using SCT.Application.Web.Models;
+using SCT.Application.Web.HtmlHelpers;
 
 namespace SCT.Application.UnitTest
 {
@@ -7,8 +10,32 @@ namespace SCT.Application.UnitTest
     public class UnitTestSCT
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestandoQuebraPaginacao()
         {
+            //Arrange
+
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            //Act
+
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            //Assert
+
+            Assert.AreEqual(
+                @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                + @"<a class=""btn btn-default selected btn-primary"" href=""Pagina2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                );
         }
     }
 }
--- SCT.Application.Web/HtmlHelpers/PaginacaoHelpers.cs
+++ SCT.Application.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -10,11 +10,11 @@ namespace SCT.Application.Web.HtmlHelpers
 {
     public static class PaginacaoHelpers
     {
-        public static MvcHtmlString PageLinks(HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
         {
             StringBuilder resultado = new StringBuilder();
 
-            for (int i = 1; i < paginacao.TotalPagina; i++)
+            for (int i = 1; i <= paginacao.TotalPagina; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.MergeAttribute("href", paginaUrl(i));
