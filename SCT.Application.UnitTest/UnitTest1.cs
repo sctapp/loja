219701be9f89946c55e63099453b77eacb369821
leopbarao <leopbarao@gmail.com>
2015-10-31 09:46:02 -0200

    Initial Commit

---
+++ SCT.Application.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SCT.Application.UnitTest
+{
+    [TestClass]
+    public class UnitTestSCT
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SCT.Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SCT.Application.MvcApplication" Language="C#" %>
