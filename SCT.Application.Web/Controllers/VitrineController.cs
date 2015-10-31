90ff831f5d795b21eb9d0244141583350bb15e5d
leopbarao <leopbarao@gmail.com>
2015-10-31 15:38:01 -0200

    Paginacao

---
+++ SCT.Application.Web/Controllers/VitrineController.cs
@@ -0,0 +1,27 @@
+﻿using SCT.Entity.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCT.Application.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        public int ProdutoPorPagina = 3;
+        //
+        // GET: /Vitrine/
+        public ActionResult ListProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos
+                    .OrderBy(p => p.Nome)
+                    .Skip((pagina - 1) * ProdutoPorPagina)
+                    .Take(ProdutoPorPagina);
+
+            return View(produtos);
+        }
+    }
+}
