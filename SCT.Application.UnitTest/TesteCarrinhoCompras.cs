1825f0d798933f2e1e6079a7864ea70416fdc5fd
leopbarao <leopbarao@gmail.com>
2015-11-11 08:39:56 -0200

    TestMethods

--- SCT.Application.UnitTest/TesteCarrinhoCompras.cs
+++ SCT.Application.UnitTest/TesteCarrinhoCompras.cs
@@ -12,7 +12,7 @@ namespace SCT.Application.UnitTest
         public void AdicionarItensAoCarrinho()
         {
 
-            //region Arrange
+            //Arrange
 
             Produto produto1 = new Produto
             {
@@ -47,6 +47,120 @@ namespace SCT.Application.UnitTest
             Assert.AreEqual(itens.Length, 3);
             Assert.AreEqual(itens[0].Produto, produto1);
             Assert.AreEqual(itens[1].Produto, produto2);
+        }
+
+        [TestMethod]
+        public void AdicionarItemExistenteLista()
+        {
+            //region Arrange
+
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Produto 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Produto 2"
+            };
+
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 2);
+
+            carrinho.AdicionarItem(produto2, 7);
+
+            carrinho.AdicionarItem(produto1, 10);
+
+            //Act
+            ItemCarrinho[] resultado = carrinho.ItensCarrinho.OrderBy(c => c.Produto.Nome).ToArray();
+
+            //Assert
+            Assert.AreEqual(resultado.Length, 2);
+        }
+
+        [TestMethod]
+        public void RemoverItemDoCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Produto 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Produto 2"
+            };
+
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 2);
+
+            carrinho.AdicionarItem(produto2, 7);
+
+            carrinho.RemoverItem(produto2);
+
+            Assert.AreEqual(carrinho.ItensCarrinho.Where(c => c.Produto == produto2).Count(), 0);
+        }
+
+        [TestMethod]
+        public void CalcularValorTotalCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Produto 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Produto 2",
+                Preco = 50M
+            };
+
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 2);
+
+            carrinho.AdicionarItem(produto2, 7);
+
+            decimal resultado = carrinho.ObterValorTotal();
+
+            Assert.AreEqual(resultado, 550M);
+        }
+
+        [TestMethod]
+        public void LimparCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Produto 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Produto 2",
+                Preco = 50M
+            };
+
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 2);
+
+            carrinho.AdicionarItem(produto2, 7);
+
+            carrinho.LimparCarrinho();
+
+            Assert.AreEqual(carrinho.ItensCarrinho.Count(), 0);
         }
     }
 }
