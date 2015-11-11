0e72c2e7fbad6c56d580c9a6b8bb5f6e469e6544
leopbarao <leopbarao@gmail.com>
2015-11-11 09:09:14 -0200

    CarrinhoController

---
+++ SCT.Application.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,59 @@
+﻿using SCT.Entity.Entidades;
+using SCT.Entity.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCT.Application.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        //
+        // GET: /Carrinho/
+        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                ObterCarrinho().AdicionarItem(produto, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Carrinho ObterCarrinho()
+        {
+            Carrinho carrinho = (Carrinho)Session["Carrinho"];
+
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                ObterCarrinho().RemoverItem(produto);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+    }
+}
