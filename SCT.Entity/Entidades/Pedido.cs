b81538582b664cced3a61da2762472b1e2dde58f
leopbarao <leopbarao@gmail.com>
2016-01-24 12:06:42 -0200

    Entidade Pedido

---
+++ SCT.Entity/Entidades/Pedido.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCT.Entity.Entidades
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage = "Informe seu nome")]
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "CEP:")]
+        public string CEP { get; set; }
+
+        [Required(ErrorMessage = "Informe seu endereço")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Informe sua cidade")]
+        [Display(Name = "Cidade:")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe seu bairro")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Informe seu estado")]
+        [Display(Name = "Estado:")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "Informe seu email")]
+        [Display(Name = "Email:")]
+        [EmailAddress(ErrorMessage = "Email invalido")]
+        public string Email { get; set; }
+
+        public bool EmbrulhaPresente { get; set; }
+    }
+}
